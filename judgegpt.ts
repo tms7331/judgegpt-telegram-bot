2b1f915ea33d8eaedfeecfb13606544f39f3d91e
Josh Klopfenstein <git@joshklop.com>
2024-08-01 19:29:50 -0500

    config

--- judgegpt.ts
+++ judgegpt.ts
@@ -1,12 +1,23 @@
 import { Bot, InlineKeyboard } from 'grammy';
 import OpenAI from "openai";
+import * as dotenv from "dotenv";
+dotenv.config();
 
-const bot = new Bot(bot_key);
-const openai = new OpenAI(chatgpt_key);
+const botKey = process.env.JUDGE_BOT_KEY;
+if (botKey == undefined) {
+  process.exit(1);
+}
+const chatgptKey = process.env.JUDGE_CHATGPT_KEY;
+if (chatgptKey == undefined) {
+  process.exit(1);
+}
+
+const bot = new Bot(botKey);
+const openai = new OpenAI({apiKey: chatgptKey});
 
 
 // Command listener for "/new"
-bot.command('new', (ctx) => {
+bot.command('new', (ctx: any) => {
   // Extract the command and arguments
   const text = ctx.message?.text || '';
   const args = text.split(' ').slice(1); // Get all words after the command
@@ -22,4 +33,3 @@ bot.command('new', (ctx) => {
 
 
 bot.start();
-
